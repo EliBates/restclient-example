b0449f60c9590130216278b2eaa2fe522b7d7168
EliBates <eli.m.bates@gmail.com>
2021-06-21 14:40:04 +0200

    Initial commit

---
+++ src/main/java/com/elibates/restclient/services/JokeService.java
@@ -0,0 +1,2 @@
+package com.elibates.restclient.services;public class JokeService {
+}
---
+++ src/test/java/com/elibates/restclient/RestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elibates.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
